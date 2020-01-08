9fe2080e3a741dc3850667a1dfa3dbd3646c9416
eharbers <erik.harbers64@gmail.com>
2020-01-08 23:52:22 +0100

    playValidate aangevuld voor eerste twee kaarten

--- cbb.js
+++ cbb.js
@@ -6,13 +6,20 @@ cards.init({table:'#card-table'});
 deck = new cards.Deck(); 
 //By default it's in the middle of the container, put it slightly to the side
 deck.x -= 150;
+deck.y -= -40;
 
 //cards.all contains all cards, put them all in the deck
 deck.addCards(cards.all); 
 //No animation here, just get the deck onto the table.
 deck.render({immediate:true});
 
-//Now lets create a couple of hands, one face down, one face up.
+// discardPile voor gespeelde kaarten.
+// deze moeten later weer worden toegevoegd aan deck
+discardPile = new cards.Deck({faceUp:false});
+discardPile.x -= 150;
+discardPile.y -= 80;
+
+//Now lets create a couple of hands, one face down (now up), one face up.
 upperhand = new cards.Hand({faceUp:true, y:60});
 lowerhand = new cards.Hand({faceUp:true, y:440});
 
@@ -23,9 +30,16 @@ lowerPlay = new cards.Hand({faceUp:true, y:300});
 var turnLower = true; // in dev & test begint Lower met pitchen/veld
 var turnUpper = false;
 
+var objHand = lowerhand; // het zetten van de eerste speler, omdat
+var objPlay = lowerPlay; // er nog geen functie voor de ad random selectie is
+var atBatStatus = 'pitch';
+
+numStrikes = 0;
+numBalls = 0;
+
 var endOfGame = false;
 
-
+// activeren van het spel met de DEAL button (of Play Ball)
 $('#deal').click(function() {
 	//Deck has a built in method to deal to hands.
 	$('#deal').hide();
@@ -42,12 +56,9 @@ $('#deal').click(function() {
 window.requestAnimationFrame(playBall);
 
 function playBall(){
-		//start slagbeurt
-		playCard();
-		if (lowerPlay.length !=0 && upperPlay.length !=0) {
-			console.log ('beide hebben een kaart gelegd');
-			//endOfGame = true;
-		}
+		//slagbeurt 
+		console.log('playBall atBatStatus: ', atBatStatus);
+		playCard(objHand, objPlay);		
 	if (endOfGame === true) { // dit stukje zorgt voor de herhaling, todat endOfGame 'waar' is
 		gameOver();
 	} else {
@@ -56,64 +67,143 @@ function playBall(){
 }
 // einde game-loop
 
-//When you click on the top card of a deck, a card is added
-//to your hand
-/* deck.click(function(card){
-	if (card === deck.topCard()) {
-		lowerhand.addCard(deck.topCard());
-		lowerhand.render();
-	}
-}); */
 
-function playCard() {
-
-/* mogelijkheid:
+/* TODO mogelijkheid:
 playCard aansturen met welke hand er gespeeld mag worden:
 playCard(lower) of playCard(upper) 
 in functie afvangen met playCard(obj) {}
 
-de click functie is er dan nog maar eentje, nl:
-
-obj.click(function(card) {
-
-});
-
-het wisselen van speler gebeurd dan wellicht in de playValidate
-
-renderen van de kaarten buiten playValidate
+renderen van de kaarten buiten playValidate ???
 zoals die website (www.sitepoint.com/quick-tip-game-loop-in-javascript/) eigenlijk ook doet
 */
 
-lowerhand.click(function(card) {
-	if (turnLower === true) {
-		console.log ('turnLower: ', turnLower);
-		lowerPlay.addCard(card);
-		playRender();
-		turnLower = false;
-		turnUpper = true;
-		playValidate();
-	} else {
-		console.log ('turnLower else: ', turnLower);
-	}
-});
-
-upperhand.click(function(card) {
-	if (turnUpper === true) {
-		console.log('turnUpper: ', turnUpper);
-		upperPlay.addCard(card);
-		playRender();
-		turnUpper = false;
-		turnLower = true;
-		playValidate();
-	} else {
-		console.log('turnUpper else: ', turnUpper);
-	}
-});
+// functie om kaart te klikken uit de hand die aan de beurt is.
+function playCard(objHand, objPlay) { // kan dat ook op een 'naam' van het object-manier??
+										// nu ontstaat er volgens mij een tweede object...
+										// wellicht terugkopieren?
+	objHand.click(function(card) {
+		if (turnLower === true) {
+			console.log ('turnLower: ', turnLower, 'turnUpper: ', turnUpper);
+			objPlay.addCard(card);
+			playRender();
+			playValidate();
+		} else {
+			console.log ('turnLower else: ', turnLower);
+		}
+	});
 }
 
 
 function playValidate() {
 	console.log('inside playValidate');
+	// feitelijke controle op de kaart die als laatste aan lowerPlay of upperPlay is toegevoegd
+	// dit gaat dan met topCard() gebeuren
+	switch (atBatStatus) {
+		case 'pitch':
+			console.log('atBatStatus: ', 'pitch');
+			console.log(objPlay.topCard().name);
+			if (objPlay.topCard().rank >= 11) {
+				console.log('BALL !!'); // onnozel als je geen 4-wijd wil gooien...
+				numBalls += 1
+				console.log('Balls: ', numBalls, ' Strikes: ', numStrikes); // naar scoreboard
+				// cleanup playing hands !!
+				moveCards(objPlay, discardPile);
+				refillHand(objHand);
+				// en die andere ook
+				moveCards(upperPlay, discardPile); // die is waarschijnlijk leeg...
+				refillHand(upperPlay);
+				atBatStatus = 'pitch';
+			} else {
+				atBatStatus = 'swing';
+				refillHand(objHand); // eerst aanvullen
+				changePlayer(); // dan pas van speler wisselen
+			}
+			console.log('atBatStatus is now: ', atBatStatus);
+			break;
+		case 'swing':
+			console.log('atBatStatus: ', 'swing');
+			console.log(objPlay.topCard().name);
+			if (objPlay.topCard().suit != lowerPlay.topCard().suit) { // deze vergelijkig is niet goed...
+				console.log('STRIKE');
+				// cleanup playing hands !!
+				numStrikes += 1;
+				console.log('Balls: ', numBalls, ' Strikes: ', numStrikes); // naar scoreboard
+				moveCards(objPlay, discardPile);
+				// en die andere ook !!
+				moveCards(lowerPlay, discardPile);
+				atBatStatus = 'pitch';
+				refillHand(objHand);
+				changePlayer();
+			} else if (objPlay.topCard().rank < lowerPlay.topCard().rank) {
+					console.log('BALL !!');
+					numBalls += 1
+					console.log('Balls: ', numBalls, ' Strikes: ', numStrikes); // naar scoreboard
+					// cleanup playing hands !!
+					moveCards(objPlay, discardPile);
+					refillHand(objHand);
+					// en die andere ook
+					moveCards(lowerPlay, discardPile); // die is waarschijnlijk leeg...
+					refillHand(lowerPlay);
+					atBatStatus = 'pitch';
+			} else if (objPlay.topCard().rank >=11) {
+					if (numStrikes <2) {
+						console.log('STRIKE !!');
+						numStrikes +=1;
+						console.log('Balls: ', numBalls, ' Strikes: ', numStrikes); // naar scoreboard
+						// cleanup playing hands !!
+						moveCards(objPlay, discardPile);
+						refillHand(objHand);
+						// en die andere ook
+						moveCards(lowerPlay, discardPile); // die is waarschijnlijk leeg...
+						refillHand(lowerPlay);
+						atBatStatus = 'pitch';
+					} else {
+						console.log('FOUL');
+						console.log('Balls: ', numBalls, ' Strikes: ', numStrikes); // naar scoreboard
+						// cleanup playing hands !!
+						moveCards(objPlay, discardPile);
+						refillHand(objHand);
+						// en die andere ook
+						moveCards(lowerPlay, discardPile); // die is waarschijnlijk leeg...
+						refillHand(lowerPlay);
+						atBatStatus = 'pitch';
+					}
+			} else {
+				console.log('connect');
+				atBatStatus = 'connect';
+				refillHand(objHand);
+			}							
+			console.log('atBatStatus is now: ', atBatStatus);
+			break;
+		default:
+			console.log('DEFAULT');
+			endOfGame = true;
+	}
+}
+
+// beurt wisselen
+function changePlayer() {
+	if (turnLower) {
+		turnUpper = true;
+		objHand = upperhand;
+		objPlay = upperPlay;
+	} else {
+		turnLower = true;
+		objHand = lowerhand;
+		objPlay = lowerPlay;
+	}
+}
+
+	/*if (lowerPlay.length !=0 && upperPlay.length !=0) {
+		console.log ('beide hebben een kaart gelegd');
+		endOfGame = true;
+	} */
+
+
+// functie om de Hand aan te vullen met een kaart van deck
+function refillHand(objHand) {
+	objHand.addCard(deck.topCard());
+	playRender();
 }
 
 function playRender() {
@@ -125,6 +215,21 @@ function playRender() {
 	lowerPlay.render();
 }
 
+// functie om een HAND met kaarten te verplaatsten
+// hierin in blijkbaar niet voorzien in card.js
+function moveCards(from, to) {
+	for (let i=0; i < from.length; i++) {
+		to.push(from[i]);
+		from.splice(i,1);
+		i--;
+	}
+	from.render();
+	to.render();
+}
+
+
+
 function gameOver() {
 	console.log('GAME OVER');
 }
+
